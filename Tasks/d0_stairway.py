8b063b8879e662f4aed103ccc618eb6cb62a298b
annlesss <leshchineranna@gmail.com>
2022-09-14 20:47:18 +0300

    практика 14.09

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -9,4 +9,31 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
     :return: minimal cost of getting to the top
     """
     print(stairway)
-    return 0
+    cost = stairway.copy()
+    for i in range(2, len(stairway)): # первые два элемента уже известны
+        cost[i] += min(cost[i - 1], cost[i - 2]) # сравниваем с предыдущей и предпредыдущей и складываем стоимость
+    return cost[-1]
+
+def stairway_path_r(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
+
+    cost = [''] * len(stairway)
+    cost[0] = stairway[0]
+    cost[1] = stairway[1]
+
+    def rec(n: int) -> Union[float, int]:
+        if cost[n] != '':
+            return cost[n]
+        if n < 2:
+            return cost[n]
+        cost[n] = min(rec(n-1) + stairway[n], rec(n - 2) + stairway[n])
+        return cost[n]
+
+    return rec(len(stairway) - 1)
+
+
+if __name__ == "__main__":
+    test_st = [1, 3, 1, 5, 2, 7, 7, 8, 9, 4, 6, 3]
+    print(stairway_path(test_st))
+    print(stairway_path_r(test_st))
+    test_st = [1, 3, 5, 3, 4, 5, 8, 8, 2, 4, 6, 3]
+    print(stairway_path(test_st))
--- Tasks/d1_horse.py
+++ Tasks/d1_horse.py
@@ -1,10 +1,37 @@
-def calculate_paths(shape: (int, int), point: (int, int)) -> int:
+def calculate_paths(shape: (int, int), point: (int, int), start_point=(1, 1)) -> int:
     """
     Given field with size rows*cols count available paths from (0, 0) to point
 
     :param shape: tuple of rows and cols (each from 1 to rows/cols)
     :param point: desired point for horse
+    :param start_point:
     :return: count of paths from (1, 1) to (point[0], point[1]) (numerating from 0, so (0, 0) - left bottom tile)
     """
+    """
+    path_rec(i - 1, j - 2),
+    path_rec(i - 2, j - 1),
+    path_rec(i - 2, j + 1),
+    """
     print(shape, point)
-    return 0
+    table = [[0 for i in range(shape[1])] for i in range(shape[0])]
+    table[start_point[0]][start_point[1]] = 1
+
+    def path_rec(i: int, j: int) -> int:
+        if 0 <= i <= shape[0] - 1 and 0 <= j <= shape[1] - 1:
+            if table[i][j]:
+                return table[i][j]
+            table[i][j] = sum(( path_rec(i + 1, j - 2),
+                                path_rec(i - 1, j - 2),
+                                path_rec(i - 2, j - 1),
+                                path_rec(i - 2, j + 1),
+                                ))
+            return table[i][j]
+
+        return 0
+
+    return path_rec(*point)
+
+
+if __name__ == "__main__":
+    print(calculate_paths((8, 8),(7, 7)))
+    assert calculate_paths((8, 8), (7, 7))
