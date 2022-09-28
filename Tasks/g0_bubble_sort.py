d6ff987b5298cab12114eb98da22a45311c7fbad
annlesss <leshchineranna@gmail.com>
2022-09-28 20:35:29 +0300

    практика 28.09

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -8,4 +8,10 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
+    n = len(container)
+    for i in range(n-1):
+        for j in range(n - i - 1):
+            if container[j] > container[j + 1]:
+                container[j], container[j + 1] = container[j + 1], container[j]
+
     return container
--- Tasks/g1_merge_sort.py
+++ Tasks/g1_merge_sort.py
@@ -8,4 +8,42 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
+    if len(container) == 1:
+        return container
+
+    mid = len(container) // 2
+    left = container[:mid]
+    right = container[mid:]
+    sort(left)
+    sort(right)
+
+    return merge(container, left, right)
+
+
+def merge(container: List[int], left: List[int], right: List[int], i=0, j=0, k=0) -> List[int]:
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            container[k] = left[i]
+            i += 1
+        else:
+            container[k] = right[j]
+            j += 1
+        k += 1
+
+    while i < len(left):
+        container[k] = left[i]
+        i += 1
+        k += 1   # k позиция в контейнере
+
+    while j < len(right):
+        container[k] = right[j]
+        j += 1
+        k +=1
+
     return container
+
+
+if __name__ == "__main__":
+    data = list(range(10, -1, -1))
+    print(sort(data))
